36e0f3122398ec78c53e9cf55c30c0a2be98796b
Vishal Apte <vishalapte@gmail.com>
2024-05-01 05:32:37 +0530

    Update copy_dotfiles.sh

--- shell/copy_dotfiles.sh
+++ shell/copy_dotfiles.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-giturl="https://raw.githubusercontent.com/vishalapte/foundation/main"
+giturl="https://raw.githubusercontent.com/quxdev/qconfig/main/shell"
 
 # echo "Run this command from the prompt"
-# echo '/bin/bash -c "$(curl -fsSL '${giturl}'/shell/copy_shell_scripts.sh)"'
+# echo '/bin/bash -c "$(curl -fsSL '${giturl}'/copy_shell_scripts.sh)"'
 
 for f in bash_login bash_aliases qux_macosx qux_images qux_python; do
   if [[ -f $HOME/.${f} ]]; then
     echo "Found. Not copying ${f}"
   else
     echo "Not found. Copying ${f}"
-    curl -fsSL ${giturl}/shell/${f} > $HOME/.${f}
+    curl -fsSL ${giturl}/${f} > $HOME/.${f}
   fi
 done
